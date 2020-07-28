b2cdcd2470b0a963d2390998eff3c8bfb54a6d26
LuisJurado99 <luisjurado9924@gmai.com>
2020-07-27 23:19:37 -0500

    Inicio_de_Clases

---
+++ ClasesKotlin/src/unam/examples/Camera.kt
@@ -0,0 +1,11 @@
+package unam.examples
+
+class Camera {
+    var isOn : Boolean = false
+    fun turnOn(){
+        this.isOn = true
+    }
+    fun turnOff(){
+        this.isOn = false
+    }
+}
---
+++ ClasesKotlin/src/unam/mains/Main.kt
@@ -0,0 +1,6 @@
+package unam.mains
+import unam.examples.Camera
+fun main(args: Array<String>) {
+    var  camera = Camera()
+    camera.turnOn()
+}
